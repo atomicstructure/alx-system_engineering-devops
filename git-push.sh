f7b1161e69deb1a6984218bcb5b622f014ba3639
oluwafemi <ogundarearnold@gmail.com>
2023-02-15 18:44:43 +0100

    commit

--- git-push.sh
+++ git-push.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-    git add .
-    echo "Insert your commit >"
+  git add .
+    echo "Insert your commit message >"
     read -r commit
-    git commit -m"${commit}"
+    git commit -m commit
     echo "Pull or Push >"
     read -r answer
     if [[ $answer == "pull" ]];
     then
         git pull
-        echo "Done with pulling, do you wanna push your files (y/n)?"
+        echo "Done with pulling, will you like to push your files (y/n)?"
         read -r pushing
         if [[ $pushing == "y" ]];
         then
